bcfe56529bda820f42885aec9f5b00e1c0d69049
Erin Kahn <erinnicolekahn@gmail.com>
2018-07-17 19:52:42 -0400

    js nav open...need to style

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,10 @@
-"use strict";console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+'use strict';
+
+console.log('Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you\'re working in the right files.');
+
+// nav menu mobile
+
+document.querySelector('.burger a').addEventListener('click', function () {
+    document.querySelector('.burger').classList.toggle('openNav');
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 console.log(`Hello World from app.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+// nav menu mobile
+
+document.querySelector('.burger a').addEventListener('click', function() {
+    document.querySelector('.burger').classList.toggle('openNav');
+})
